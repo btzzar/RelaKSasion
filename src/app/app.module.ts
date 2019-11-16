21c7fbba642bb2dda15b5fe2af7c32a796e1597a
btzzar <luka.m.banovic@gmail.com>
2019-11-16 21:17:11 +0100

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TrainingComponent } from './training/training.component';
+import { PreviousTrainingComponent } from './training/previous-training/previous-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SignupComponent,
+    LoginComponent,
+    WelcomeComponent,
+    TrainingComponent,
+    PreviousTrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent
   ],
   imports: [
     BrowserModule,
